f8238213405eafa3dbd3a078b5c6d9a9312d9cd7
Rohan van Wijk <rohan_10@live.nl>
2020-07-04 23:37:54 +0200

    basis

--- carousel.js
+++ carousel.js
@@ -1,43 +1,44 @@
-const carouselSlides = document.querySelector(".carousel").children;
-const totalSlides = carouselSlides.length;
+const modal = document.querySelector(".JS-modal");
+const thumbnails = document.querySelectorAll(".thumbnail-image");
 // Buttons
 const btnNext = document.querySelector(".JS-next");
 const btnPrev = document.querySelector(".JS-prev");
+const btnClose = document.querySelector(".JS-close");
 
-var currentSlide = 0;
+let imagesList = []
+let imageActive = ""
 
-// Button listeners
-btnNext.addEventListener("click", () => {
-  moveSlide(true);
-});
-
-btnPrev.addEventListener("click", () => {
-  moveSlide(false);
-});
-
-function moveSlide(forward) {
-  if (forward) {
-    currentSlide++;
-    if (currentSlide == totalSlides) {
-      currentSlide = 0;
-    }
-  } else {
-    currentSlide--;
-    if (currentSlide < 0) {
-      currentSlide = totalSlides - 1;
-    }
-  }
-  console.log(currentSlide);
-  removeActiveClass("active");
-  setActiveClass("active", currentSlide);
+for(let i = 0; i < thumbnails.length; i++) {
+  const image = thumbnails[i].children[0].currentSrc;
+  imagesList.push(image);
+  thumbnails[i].addEventListener("click", function() {
+    imageActive = thumbnails[i].children[0].currentSrc;
+    modal.children[0].children[0].src = imageActive;
+    modal.style.display = "block";
+  });
 }
 
-function removeActiveClass(className) {
-  for (i = 0; i < totalSlides; i++) {
-    carouselSlides[i].classList.remove(className);
+btnClose.addEventListener("click", function() {
+  modal.style.display = "none";
+});
+
+btnNext.addEventListener("click", function() {
+console.log(imagesList);
+for(let i = 0; i < imagesList.length; i++) {
+  if(imageActive == imagesList[i]) {
+    const next = imagesList[i + 1];
+    setImage(next);
   }
 }
 
-function setActiveClass(className, slide) {
-  carouselSlides[slide].classList.add(className);
+});
+
+btnPrev.addEventListener("click", function() {
+console.log(imageActive);
+
+});
+
+function setImage(image) {
+  modal.children[0].children[0].src = image;
+  imageActive = image;
 }
